25bf0499df1227f1527897a21b45c538f128bcc0
Reminem <benaremer@gmail.com>
2018-02-09 16:54:32 -0500

    Logout logo change

--- app/src/main/java/app/haven/haven/MainActivity.java
+++ app/src/main/java/app/haven/haven/MainActivity.java
@@ -1,10 +1,13 @@
 package app.haven.haven;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button sidebartest = findViewById(R.id.button);
+        sidebartest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SideBar.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
